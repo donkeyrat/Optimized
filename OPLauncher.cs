329565dae00117c55a8154ee69aea8a5f7aa32d3
donkeyrat <darklordbananas@gmail.com>
2022-11-03 22:10:40 -0700

    added stuff

---
+++ OPLauncher.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx;
+
+namespace Optimized_ {
+
+	[BepInPlugin("teamgrad.optimized", "Optimized!", "1.0.0")]
+	public class OPLauncher : BaseUnityPlugin {
+
+		public OPLauncher() { OPBinder.UnitGlad(); }
+	}
+}
